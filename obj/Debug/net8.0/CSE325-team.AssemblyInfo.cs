abbbc0c4e8c10e3f1a41785a67c07f891fc72ef8
Erik Burton <erikburton1@gmail.com>
2025-07-21 06:10:10 -0600

    B.1.Create Car Model.

--- obj/Debug/net8.0/CSE325-team.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSE325-team.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSE325-team")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f9330a40614bbb2fd6984be5aa1c2cfdc41c7f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+797a7ea90378e21a610f749040af4c26be89aba2")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSE325-team")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSE325-team")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
