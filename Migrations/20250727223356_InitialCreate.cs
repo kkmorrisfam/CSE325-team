a840b53097837eee326a5353be443dc5afbfc742
Erik Burton <erikburton1@gmail.com>
2025-07-28 15:37:44 -0600

    Corrected namespace to CSE325_team.

--- Migrations/20250727223356_InitialCreate.cs
+++ Migrations/20250727223356_InitialCreate.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace CSE325team.Migrations
+namespace CSE325_team.Migrations
 {
     /// <inheritdoc />
     public partial class InitialCreate : Migration
