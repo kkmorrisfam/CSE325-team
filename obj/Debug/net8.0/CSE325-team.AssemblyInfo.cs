0cae6320968f3af6dc3fba20687b7f2a69cda012
centervalentine <centervalentine@gmail.com>
2025-07-29 09:12:29 -0600

    Merge update. remote-tracking branch 'origin/main' into dpv-w1

--- obj/Debug/net8.0/CSE325-team.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSE325-team.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSE325-team")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+061c6121f889a86c8c812bbd8a8b3cf853c4092f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec1ae64295efc942db1ff8f1067916d11860fcdf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSE325-team")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSE325-team")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
