41611013bb9118fcffc587872ade905471935a72
Kerri Morris <kkmorrisfam@hotmail.com>
2025-07-30 20:44:21 -0700

    fix migration

--- Migrations/20250730021149_InitialCreate.cs
+++ Migrations/20250730021149_InitialCreate.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace CSE325_team.Migrations
 {
     /// <inheritdoc />
-    public partial class UpdateMyDbVehBookings : Migration
+    public partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
